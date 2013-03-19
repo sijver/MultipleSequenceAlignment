6533155d5a61f80a93e8c64a8fe6e8206e0c32d8
Mark <sijver@gmail.com>
2013-03-20 00:10:29 +0100

    static tree construction method has been added

--- src/core/msa/Cluster.java
+++ src/core/msa/Cluster.java
@@ -22,15 +22,34 @@ public class Cluster {
         return clusterObjects;
     }
 
-    public static List<Integer> getAllSubclustersValues(Cluster cluster) {
+    public static List<Integer> getClusterProteins(Cluster cluster) {
         List<Integer> integers = new LinkedList<Integer>();
         for (Object o : cluster.getClusterObjects()) {
             if (o.getClass() == Integer.class) {
                 integers.add((Integer) o);
             } else if (o.getClass() == Cluster.class) {
-                integers.addAll(Cluster.getAllSubclustersValues((Cluster) o));
+                integers.addAll(Cluster.getClusterProteins((Cluster) o));
             }
         }
         return integers;
+    }
+
+    public static String getClusterTree(Cluster cluster, int deepness){
+        StringBuilder clusterTree = new StringBuilder();
+
+        clusterTree.append("\n");
+        for(int i = 0; i < deepness; i++){
+            clusterTree.append("  ");
+        }
+        clusterTree.append("|_");
+        for(Object o : cluster.getClusterObjects()){
+            if(o.getClass() == Cluster.class){
+                clusterTree.append(((Cluster)o).getClusterTree((Cluster)o, deepness + 1));
+            } else {
+                clusterTree.append(o);
+            }
+        }
+
+        return clusterTree.toString();
     }
 }
