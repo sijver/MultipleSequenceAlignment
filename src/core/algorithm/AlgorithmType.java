20f9ad9889faca3b379ac6bc84e1cba703b31a0f
Mark <sijver@gmail.com>
2013-03-20 00:09:01 +0100

    alignment algorithms has been enumerated

---
+++ src/core/algorithm/AlgorithmType.java
@@ -0,0 +1,11 @@
+package core.algorithm;
+
+/**
+ * Created with IntelliJ IDEA.
+ */
+public enum AlgorithmType {
+    NEEDLEMAN_WUNSCH_LINEAR,
+    NEEDLEMAN_WUNSCH_AFFINE,
+    SMITH_WATERMAN_LINEAR,
+    SMITH_WATERMAN_AFFINE;
+}
